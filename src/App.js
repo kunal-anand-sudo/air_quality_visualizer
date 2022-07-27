c9d4a9c3957121f1e301623a6e6bd4375468d737
kunal-anand-sudo <65329319+kunal-anand-sudo@users.noreply.github.com>
2022-07-27 23:23:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import mapboxgl, { Marker } from "mapbox-gl";
 import coordinateJSON from "./coordinateJSON.json";
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass =
-  require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
+mapboxgl.workerClass =require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX;
 
